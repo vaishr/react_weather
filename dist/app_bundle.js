21bd8cf8de204033df288878d690350ab32da5c7
Vaishnavi Reddy <vaireddy11@gmail.com>
2016-05-31 03:44:13 -0400

    add config for webpack and babel

---
+++ dist/app_bundle.js
@@ -0,0 +1,1 @@
+!function(t){function e(n){if(r[n])return r[n].exports;var o=r[n]={exports:{},id:n,loaded:!1};return t[n].call(o.exports,o,o.exports,e),o.loaded=!0,o.exports}var r={};return e.m=t,e.c=r,e.p="",e(0)}([function(t,e,r){t.exports=r(1)},function(t,e){var r=document.getElementById("app");r.innerHTML="test"}]);
