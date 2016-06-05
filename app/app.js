fbbe1ff9bf950939d8af3365e8815b547633fce7
Vaishnavi Reddy <vaireddy11@gmail.com>
2016-06-05 12:02:01 -0700

    add basic components

--- app/app.js
+++ app/app.js
@@ -1,13 +1,86 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var USER_DATA = {
+  firstName: "Vaishnavi",
+  lastName: "Reddy",
+  location: "San Francisco",
+  username: "vaishr",
+  picURL: "profilepic.jpg"
+};
 
-var Hello = React.createClass({
+var Name = React.createClass({
   render: function() {
     return (
-      <div>Hello World</div>
+      <div>
+        <div>{this.props.first} {' '} {this.props.last}</div>
+      </div>
     )
   }
-});
+})
 
-ReactDOM.render(< Hello />, document.getElementById('app'));
+var UserLocation = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.location}</div>
+    )
+  }
+}) 
+
+var UserName = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div>{this.props.username}</div>
+      </div>
+    )
+  }
+})
+
+var Link = React.createClass({
+  changeURL: function() {
+    window.location.replace(this.props.href);
+  },
+  render: function() {
+    return (
+      <span style={{color: 'blue', cursor: 'pointer'}} onClick={this.changeURL}>
+        {this.props.children}
+      </span>
+    )
+  }
+})
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Link href={'https://www.github.com/'+this.props.username}>
+          {this.props.username}
+        </Link> 
+      </div>
+    )
+  }
+})
+
+// var ProfilePic = React.createClass({
+//   render: function() {
+//     <div>
+//       <img src={this.props.picURL} />
+//     </div>
+//   }
+// })
+
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfileLink username = {this.props.user.username} />
+        <UserLocation location = {this.props.user.location} />
+        <UserName username = {this.props.user.username} />
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<Avatar user = {USER_DATA} />, document.getElementById('app'));
